339cc2f5c8efa5cb526f9ebd753fa1540cdba46e
Patrick Onokwai <patrick.onokwai@gmail.com>
2021-01-02 17:14:40 +0100

    complete tutorial

--- todoapp/src/app.ts
+++ todoapp/src/app.ts
@@ -1,3 +1,42 @@
+interface Todo{
+  description: string,
+  done: boolean
+}
+
 export class App {
-  public message = 'Hello World!';
+  heading = "Todo";
+  todos: Todo[] = [];
+  todoDescription = '';
+
+  addTodo(){
+    if(this.todoDescription){
+      this.todos.push({
+        description: this.todoDescription,
+        done: false
+      });
+      
+      this.todoDescription = '';
+    }
+  }
+
+  removeTodo(todo){
+    const index = this.todos.indexOf(todo);
+    if(index !== 1){
+      this.todos.splice(index, 1);
+    }
+  }
+
+  cloneTodo(todo){
+    const index = this.todos.indexOf(todo);
+    if(index !== -1){
+      this.todos.push(todo);
+    }   
+  }
+
+  updateTodo(todo){
+    console.log(todo);
+    this.todoDescription = todo.todoDescription;
+    this.heading = "Updating";
+    this.removeTodo(todo);
+  }
 }
